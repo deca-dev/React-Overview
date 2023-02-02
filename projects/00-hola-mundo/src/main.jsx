15327c04e8f20bfae53b0ce606e4e0fa39b39e40
David Castro <david.castroanaya@gmail.com>
2023-02-01 17:16:18 -0800

    First commit

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.Fragment>
+    <button>Hola Btn 1</button>
+    <button>Hola Btn 2</button>
+  </React.Fragment>
+)
